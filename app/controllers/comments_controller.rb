74e66eb58d362073e1d25dbc50da81a34bcf7848
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 23:17:54 +0200

    Added destroy method to  comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,15 @@ class CommentsController < ApplicationController
         redirect_to todo_path(@todo)
     end
 
+    def destroy
+        @todo = Todo.find(params[:todo_id])
+        @comment = @todo.comments.find(params[:id])
+
+        @comment.destroy
+
+        redirect_to todo_path(@todo), status: 303
+    end
+
     private
     def comment_params
         params.require(:comment).permit(:person, :body)
