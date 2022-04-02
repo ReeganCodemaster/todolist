0bd2e18d1f86dadbe056f97692e15435e8abfc6b
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 22:46:53 +0200

    Added Comment Model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,6 @@
 class Todo < ApplicationRecord
-
+    has_manny :comments
+    
     validates :title , presence: true, length: {minimum:5, maximum:15}
     validates :body, presence:true, length: {minimum:20}
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_153242) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_02_204520) do
+  create_table "comments", force: :cascade do |t|
+    t.string "person"
+    t.text "body"
+    t.integer "todo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_comments_on_todo_id"
+  end
+
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_153242) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "todos"
 end
