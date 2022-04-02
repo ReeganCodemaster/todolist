e112f91ed1ac78c0606ed2c8f21e245cc4d5ac75
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 18:14:49 +0200

    Added validations and erros to Todo Model fields

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ApplicationRecord
+
+    validates :title , presence: true, length: {minimum:5, maximum:15}
+    validates :body, presence:true, length: {minumum:20}
+
+    VALID_STATUSES = ['in-progress', 'done']
+
+    validates :status , inclusion:{in:VALID_STATUSES}
 end
