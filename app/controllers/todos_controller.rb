c0aef5416d414bb943b7e0b63e0ee01d79127e11
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 18:03:03 +0200

    + new,create meth's in todos cont & strong params

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,23 @@ class TodosController < ApplicationController
     def show
         @todo = Todo.find(params[:id])
     end
+
+    def new 
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        
+        if todo.save
+            redirect_to @todo
+        else
+            render :new , status: :unprocessable_entity
+        end
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:title, :body, :status)
+    end
 end
