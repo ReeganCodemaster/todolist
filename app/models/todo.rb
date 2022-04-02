1cc0a924ad41b9af6d3ad11e252ac325f9a0d791
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 22:59:49 +0200

    fixed syntax error

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ApplicationRecord
-    has_manny :comments
-    
+    has_many :comments
+
     validates :title , presence: true, length: {minimum:5, maximum:15}
     validates :body, presence:true, length: {minimum:20}
 
