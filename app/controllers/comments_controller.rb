dbfebd66d849215e6172e3f9f20b789ee9a186b6
ReeganCodemaster <reeganviljoen@outlook.com>
2022-04-02 22:51:50 +0200

    Added update method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+    def update
+        @todo = Todo.find(params[:id])
+        @comment = @todo.comments.create()
+
+        redirect_to todo_path(@todo)
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:person, :body)
+    end
 end
